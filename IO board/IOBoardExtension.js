b2ce1fcd984b248c04cb2880644adf09b21744a8
DeqingSun <iamseer@gmail.com>
2015-02-09 11:03:47 -0500

    clean code with workaround

--- IO board/IOBoardExtension.js
+++ IO board/IOBoardExtension.js
@@ -1,13 +1,10 @@
-// makeSenseTest.js
-// Make!Sense Scrtch Extension
+// IOboardExtension.js
+// IO board Extension
 //
-// modified by Kreg Hanning, January 2015
+// Deqing Sun, January 2015
 //
-// modified version of joystickExtension.js
-// Shane M. Clements, November 2013
 //
-// This is a Scratch Extension for the Make!Sense Max board
-  
+// This is a Scratch Extension for IO board
 (function() {
 
   var MAKESENSE = {
@@ -29,97 +26,107 @@
   var input = null;
   var poller = null;
   var ext = this;
+  var poll_needed = 0;
 
   // Converts a byte into a value of the range -1 -> 1 with two decimal places of precision
-  function convertByteStr(byte) { return (parseInt(byte, 16)); }
+  function convertByteStr(byte) {
+    return (parseInt(byte, 16));
+  }
 
   function stopPolling() {
-    if(poller) clearInterval(poller);
+    if (poller) clearInterval(poller);
     poller = null;
   }
 
   ext.readMakeSense = function(name) {
-    return input[CHANNELS.indexOf(name)+2];
+    return input[CHANNELS.indexOf(name) + 2];
   };
 
   ext.whenMakeSense = function(name, op, val) {
-    var sensorVal = input[CHANNELS.indexOf(name)+2];
+    var sensorVal = input[CHANNELS.indexOf(name) + 2];
     if (op == '>') return sensorVal > val
     else if (op == '<') return sensorVal < val;
     else if (op == '=') return sensorVal == val;
   };
-  
-  
-  ext.IO_digi_out  = function(name, hl_str) {
+
+
+  ext.IO_digi_out = function(name, hl_str) {
     var channel_id = CHANNELS.indexOf(name);
-    var hl = (hl_str=='HIGH')?"H":"L";
-    if (device){
-	  console.log("digi out");	
+    var hl = (hl_str == 'HIGH') ? "H" : "L";
+    if (device) {
+      console.log("digi out");
       var bytes = new Uint8Array(16);
-	  bytes[0]=1;
-      bytes[1]=hl.charCodeAt(0);
-      bytes[2]=channel_id+"0".charCodeAt(0);
+      bytes[0] = 1;
+      bytes[1] = hl.charCodeAt(0);
+      bytes[2] = channel_id + "0".charCodeAt(0);
       var ret_len = device.write(bytes.buffer);
-	  console.log(ret_len);
+      console.log(ret_len);
     }
   };
-  
-  
-  
-  
-  
-  
-  
+
+
+
 
   ext._deviceConnected = function(dev) {
-  
-	//console.log("dev conn");
-  
-    if(device) return;
+
+
+
+    if (device) return;
     //if(dev.info['interface_number'] != 1) return;
+    console.log("dev conn");
     device = dev;
     device.open();
     //device.write(ab.buffer);
 
     poller = setInterval(function() {
-      var input_raw = device.read(16);
-	  if (input_raw){
-		input = new Uint8Array(input_raw);
-	  }
+      if (poll_needed > 0) {
+        var input_raw = device.read(16);
+        if (input_raw) {
+          input = new Uint8Array(input_raw);
+        }
+        poll_needed--;
+      }
       //device.write(ab.buffer);
     }, 20);
     //setInterval(function() { console.log(input); }, 100);
   };
 
   ext._deviceRemoved = function(dev) {
-    if(device != dev) return;
+    if (device != dev) return;
+    console.log("dev remove");
     device = null;
     stopPolling();
   };
 
   ext._shutdown = function() {
-    if(poller) clearInterval(poller);
+    if (poller) clearInterval(poller);
     poller = null;
 
-    if(device) device.close();
+    if (device) device.close();
     device = null;
   };
 
   ext._getStatus = function() {
-    if(!device) return {status: 1, msg: 'Controller disconnected'};
-    return {status: 2, msg: 'Controller connected'};
+    if (!device) return {
+      status: 1,
+      msg: 'Controller disconnected'
+    };
+    return {
+      status: 2,
+      msg: 'Controller connected'
+    };
   };
 
   var descriptor = {
     blocks: [
-	  [' ', 'turn %m.channels to Digital Out %m.digi_hl', 'IO_digi_out', 'Channel0','HIGH'],
-	
+      [' ', 'turn %m.channels to Digital Out %m.digi_hl', 'IO_digi_out', 'Channel0', 'HIGH'],
+
       ['r', 'get Make!Sense %m.channels', 'readMakeSense', 'Channel0'],
       ['h', 'when Make!Sense %m.channels %m.ops %n', 'whenMakeSense', 'Channel0', '>', 100]
     ],
     menus: {
       channels: CHANNELS,
-	  digi_hl: ['HIGH','low'],
+      digi_hl: ['HIGH', 'low'],
       ops: ['<', '=', '>']
     }
   };
